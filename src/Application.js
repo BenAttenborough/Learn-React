752b7a41259fd001c1724302446f6fcbff5e3e34
Ben Attenborough <ben.attenborough@sfdsystems.com>
2019-03-08 10:19:16 +0000

    Add alternatives

--- src/Application.js
+++ src/Application.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ShoppingList from "./ShoppingList/ShoppingListStart";
-import './App.css';
+import "./App.css";
 
 class Application extends Component {
-	render() {
-	  return (
-		<div className="App">
-		  <ShoppingList />
-		</div>
-	  );
-	}
+  render() {
+    return (
+      <div className="App">
+        <ShoppingList />
+      </div>
+    );
   }
-  
-  export default Application;
+}
+
+export default Application;
--- src/ShoppingList/ShoppingListEnd.js
+++ src/ShoppingList/ShoppingListEnd.js
@@ -15,3 +15,36 @@ function ShoppingList(props) {
   }
 
 export default ShoppingList
+
+// Alternative 1
+
+const ShoppingList1 = (props) => {
+	console.log(props)
+	  return (
+		<div className="shoppingList">
+		  <h1>Shopping List for {props.name}</h1>
+		  <ul>
+			{props.list.map(item => {
+				return <li>{item}</li>
+			})}
+		  </ul>
+		</div>
+	  );
+  }
+
+// Alternative 2
+
+class ShoppingList1 extends React.Component {
+	render() {
+		return (
+			<div className="shoppingList">
+			  <h1>Shopping List for {props.name}</h1>
+			  <ul>
+				{props.list.map(item => {
+					return <li>{item}</li>
+				})}
+			  </ul>
+			</div>
+		  );
+	}
+  }
