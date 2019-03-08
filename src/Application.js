7ce631fb6de6745ef4b721a09dc894bfb231eea1
Ben Attenborough <ben.attenborough@sfdsystems.com>
2019-03-08 14:37:33 +0000

    Added starting point

--- src/Application.js
+++ src/Application.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 import ShoppingList from "./ShoppingList/ShoppingListStart";
 // import { Counter, CounterHook } from "./Counter/CounterStart";
 // import { Counter } from "./Counter/CounterStart";
-import Conditional from "./Conditional/Conditional";
+// import Conditional from "./Conditional/Conditional";
 // import Table from "./TableEnd/Table";
 // import { colHeadings, items } from "./TableData";
 import "./Application.css";
+
+/**
+ * Uncomment components above and below to see them in action!
+ */
 
 class Application extends Component {
   render() {
