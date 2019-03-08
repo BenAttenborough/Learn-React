34dffa0bc826f658946843dcb6783a6cc60c0b3d
Ben Attenborough <ben.attenborough@sfdsystems.com>
2019-03-08 09:42:23 +0000

    Start

---
+++ src/ShoppingList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ShoppingList() {
+	  return (
+		<div className="shopping-list">
+		  <h1>Shopping List for</h1>
+		  <ul>
+			<li>Instagram</li>
+			<li>WhatsApp</li>
+			<li>Oculus</li>
+		  </ul>
+		</div>
+	  );
+  }
+
+export default ShoppingList
